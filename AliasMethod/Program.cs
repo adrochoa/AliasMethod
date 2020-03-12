61410349e45a9cd07b068e9f97808a95fd4c8a81
Adrian <Adrian@DESKTOP-US0LCGM>
2020-03-11 19:21:29 -0500

    Add project files.

---
+++ AliasMethod/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AliasMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
